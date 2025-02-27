fd213c9008039cb8a507ca84f9ae9916597699d3
willymariino <wg.mariino@gmail.com>
2025-02-27 20:37:21 +0100

    finished second exercise

--- gioco dei dadi/script.js
+++ gioco dei dadi/script.js
@@ -4,6 +4,14 @@ let num1 = Math.round(Math.random() * 5) + 1; {
 let num2 = Math.round(Math.random() * 5) + 1; {
     console.log(num2)
 }
-
+if (num1 > num2) {
+    console.log('ha vinto il giocatore 1')
+}
+if (num2 > num1) {
+    console.log('ha vinto il giocatore 2')
+}
+else {
+    console.log('pareggio')
+}
 
 
