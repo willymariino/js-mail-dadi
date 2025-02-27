130c9de04857280f2621ee6e8ddf7ee3a18261a7
willymariino <wg.mariino@gmail.com>
2025-02-27 16:25:02 +0100

    mailing list 3

--- mail/script.js
+++ mail/script.js
@@ -13,10 +13,10 @@ const mailingList = [
 const mail = (prompt('inserisci la tua mail'));
 // controlla se la mail è presente nella lista
 
-let mailFound = false;
-for (let i = 0; mailFound < mailingList.length; i++)
-    if (mailFound < mailingList.length)
+
+for (let i = 0; mail < mailingList.length; i++)
+    if (mail < mailingList.length)
         console.log(mail)
-    else (mailFound > mailingList.length)
+    else (mail > mailingList.length)
 console.log('invitato non presente.')
 
